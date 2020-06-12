05b6a63cd9fbc999f6e90daabf6910b2cb06398d
Aadhar <tyagi.aadhar@gmail.com>
2020-06-13 00:48:49 +0530

    update container.txs

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,34 +1,17 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import './ExploreContainer.css';
 
-const Page: React.FC = () => {
+interface ContainerProps {
+  name: string;
+}
 
-  const { name } = useParams<{ name: string; }>();
-
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name={name} />
-      </IonContent>
-    </IonPage>
+    <div className="container">
+      <strong>{name}</strong>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
   );
 };
 
-export default Page;
+export default ExploreContainer;
