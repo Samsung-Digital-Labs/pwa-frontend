42234326eb252bb9554290b34c571873c7b5253a
Aadhar <tyagi.aadhar@gmail.com>
2020-06-13 03:05:22 +0530

    chane menu color

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -71,16 +71,16 @@ import {
     const location = useLocation();
   
     return (
-      <IonMenu contentId="main" type="overlay">
-        <IonContent>
-          <IonList id="inbox-list">
-            <IonListHeader>BlackBoard</IonListHeader>
-            <IonNote>user@somewhere.com</IonNote>
+      <IonMenu contentId="main" type="overlay" color = "dark">
+        <IonContent color = "dark">
+          <IonList id="inbox-list" color = "dark">
+            <IonListHeader color = "dark">BlackBoard</IonListHeader>
+            <IonNote color = "dark">user@somewhere.com</IonNote>
             {appPages.map((appPage, index) => {
               return (
-                <IonMenuToggle key={index} autoHide={false}>
-                  <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                    <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                <IonMenuToggle  key={index} autoHide={false} >
+                  <IonItem color = "dark" className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                    <IonIcon  slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
                     <IonLabel>{appPage.title}</IonLabel>
                   </IonItem>
                 </IonMenuToggle>
@@ -88,10 +88,10 @@ import {
             })}
           </IonList>
   
-          <IonList id="labels-list">
-            <IonListHeader>Labels</IonListHeader>
+          <IonList id="labels-list" >
+            <IonListHeader color = "dark">Labels</IonListHeader>
             {labels.map((label, index) => (
-              <IonItem lines="none" key={index}>
+              <IonItem color = "dark" lines="none" key={index}>
                 <IonIcon slot="start" icon={bookmarkOutline} />
                 <IonLabel>{label}</IonLabel>
               </IonItem>
