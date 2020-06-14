289b11521dfa0cbf90d3c0a6c2cf7771ba01e6ae
Mayank Arora <mayank4116@gmail.com>
2020-06-14 21:32:44 +0530

    added redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+import store from './store/store';
+
+ReactDOM.render(<Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
  
 serviceWorker.register();
---
+++ src/store/store.tsx
@@ -0,0 +1,10 @@
+import {createStore,applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import rootReducer from '../reducers/rootReducer';
+
+const createStoreWithMiddleware=applyMiddleware(promiseMiddleware)(createStore);
+
+const store=createStoreWithMiddleware(rootReducer);
+
+export default store;
+
